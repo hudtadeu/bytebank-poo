c1d79f208835d25ebe3138b864f6975a1472f417
Hudson Tadeu <hudinvesting@gmail.com>
2023-07-12 11:31:06 -0300

    Entendendo o problema do cliente

---
+++ index.js
@@ -0,0 +1,11 @@
+const clienteNome = "Ricardo";
+const Cliente1CPF = 11122233309;
+const cliente1Agencia = 1001;
+const cliente1Saldo = 0;
+
+const cliente2Nome = "Alice";
+const Cliente2CPF = 88822233309;
+const cliente2Agencia = 1001;
+const cliente2Saldo = 0;
+
+console.log(cliente1Nome);
