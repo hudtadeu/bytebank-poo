cdf33dee73ef3ef95abc0366d85d9fd481a25962
Hudson Tadeu <hudinvesting@gmail.com>
2023-07-17 19:34:24 -0300

    Composição de classes

--- ContaCorrente.js
+++ ContaCorrente.js
@@ -1,5 +1,6 @@
 export class ContaCorrente {
   agencia;
+  cliente;
   // #saldo = 0
   _saldo = 0;
 
@@ -15,5 +16,10 @@ export class ContaCorrente {
       return;
     }
     this._saldo += valor;
+  }
+
+  transferir(valor, conta) {
+    const valorSacado = this.sacar(valor);
+    conta.depositar(valorSacado);
   }
 }
