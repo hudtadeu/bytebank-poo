43b5648ce942cae9c3ee0996b03261c5f5855aaf
Hudson Tadeu <hudinvesting@gmail.com>
2023-07-19 15:32:56 -0300

    Construtores

--- ContaCorrente.js
+++ ContaCorrente.js
@@ -3,6 +3,8 @@ import { Cliente } from "./Cliente.js";
 export class ContaCorrente {
   agencia;
   _cliente;
+    // #saldo = 0
+    _saldo = 0;
 
   set cliente(novoValor) {
     if (novoValor instanceof Cliente) {
@@ -13,11 +15,14 @@ export class ContaCorrente {
   get cliente() {
     return this._cliente;
   }
-  // #saldo = 0
-  _saldo = 0;
 
   get saldo() {
     return this._saldo;
+  }
+
+  constructor(cliente, agencia) {
+    this.agencia = agencia;
+    this.cliente = cliente;
   }
 
   sacar(valor) {
