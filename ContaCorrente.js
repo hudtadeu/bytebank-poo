a5faa134b0488265cad84b54c0e8a6bae13867b3
Hudson Tadeu <hudinvesting@gmail.com>
2023-07-19 14:40:18 -0300

    Getters e Setters

--- ContaCorrente.js
+++ ContaCorrente.js
@@ -1,17 +1,33 @@
+import { Cliente } from "./Cliente.js";
+
 export class ContaCorrente {
   agencia;
-  cliente;
+  _cliente;
+
+  set cliente(novoValor) {
+    if (novoValor instanceof Cliente) {
+      this._cliente = novoValor;
+    }
+  }
+
+  get cliente() {
+    return this._cliente;
+  }
   // #saldo = 0
   _saldo = 0;
 
-  sacar(valor){
+  get saldo() {
+    return this._saldo;
+  }
+
+  sacar(valor) {
     if (this._saldo >= valor) {
       this._saldo -= valor;
       return valor;
     }
   }
-  
-  depositar(valor){
+
+  depositar(valor) {
     if (valor <= 0) {
       return;
     }
@@ -19,6 +35,7 @@ export class ContaCorrente {
   }
 
   transferir(valor, conta) {
+
     const valorSacado = this.sacar(valor);
     conta.depositar(valorSacado);
   }
